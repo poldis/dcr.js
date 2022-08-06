8be6cf704d93f1a4c00379d6df6789a286403f37
Poldi <poldi0106@gmail.com>
2022-08-06 17:03:46 +0200

    return null instead of error

--- src/structures/manager/GuildManager.ts
+++ src/structures/manager/GuildManager.ts
@@ -14,14 +14,15 @@ export default class GuildManager {
 			update: options?.update || true,
 		}
 		// if (options?.all) return await this.cache.getAll('guild', "guildId", reqOpts);
-		if (!id) throw new Error("GuildManager.get() was run without an id provided.");
+		if (!id) return null;
 		
 		const data: DbGuild = await this.cache.get('guild', id, reqOpts);
+		if (!data) return null;
 		return new Guild(this.cache, data);
 	}
 	public async del(id: String | Number, options: getOptions): Promise<Boolean> {
 		if (id) return await this.cache.del('guild', id, options);
-		throw new Error("GuildManager.del() was run without an id provided.");
+		return null;
 	}
 	public async new(data: DbGuild, options: getOptions): Promise<DbGuild> {
 		return await this.cache.set("guild", data.guildId, `INSERT INTO server VALUES (${data.id}, '${data.guildId}', '${data.lang}', '${data.premium}', ${data.reviveMsgs}, ${data.cmdsUsed}, ${data.maxRevs})`, options);
--- src/structures/manager/ReviveManager.ts
+++ src/structures/manager/ReviveManager.ts
@@ -15,16 +15,17 @@ export default class ReviveManager {
 			update: options?.update || true,
 		}
 		// if (options?.all) return await this.cache.getAll('revive', "channelId", reqOpts);
-		if (!id) throw new Error("ReviveManager.get() was run without an id provided.");
+		if (!id) return null;
 
 		const data: DbRevive = await this.cache.get('revive', id, reqOpts);
+		if (!data) return null;
 		return new Revive(this.cache, data);
 	}
 	public async del(id: String | Number, options: getOptions): Promise<Boolean> {
 		if (id) return await this.cache.del('revive', id, options);
-		throw new Error("ReviveManager.del() was run without an id provided.");
+		return null;
 	}
 	public async new(data: DbRevive, options: getOptions): Promise<DbRevive> {
-		return await this.cache.set("revive", data.channelId, `INSERT INTO revives VALUES ('${data.id}', '${data.guildId}', '${data.channelId}', '${data.role}', ${data.time}, ${data.last}, '${data.timezone}', '${data.schedule}', ${data.night}, ${data.lastMsgTime}, ${data.custom}, ${data.buttons})`, options);
+		return await this.cache.set("revive", data.channelId, `INSERT INTO revives VALUES (${data.id}, '${data.guildId}', '${data.channelId}', ${data.role ? data.role.toString() : null}, ${data.time}, ${data.last}, '${data.timezone}', '${data.schedule}', ${data.night}, DEFAULT, '${data.custom}', ${data.buttons})`, options);
 	}
 }
