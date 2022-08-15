18667301ae3aef3735258a71f258807964bd44c3
Poldi <poldi0106@gmail.com>
2022-08-15 16:52:36 +0200

    added methods

--- src/structures/Custom.ts
+++ src/structures/Custom.ts
@@ -14,5 +14,18 @@ export default class Custom {
 	public buttons: Number;
 	public tcol: Number;
 
-	public async test(): Promise<void> {}
+	public async set(embed: String, buttons: Number, tcol: Number): Promise<DbCustom | null> {
+		await this.setEmbed(embed);
+		await this.setButtons(buttons);
+		return await this.setTcol(tcol);
+	}
+	public async setEmbed(embed: String): Promise<DbCustom | null> {
+		return await this.cache.set('custom', this.reviveId, `UPDATE custom SET embed = '${embed}' WHERE reviveId = ${this.reviveId}`);
+	}
+	public async setButtons(buttons: Number): Promise<DbCustom | null> {
+		return await this.cache.set('custom', this.reviveId, `UPDATE custom SET buttons = ${buttons} WHERE reviveId = ${this.reviveId}`);
+	}
+	public async setTcol(tcol: Number): Promise<DbCustom | null> {
+		return await this.cache.set('custom', this.reviveId, `UPDATE custom SET tcol = ${tcol} WHERE reviveId = ${this.reviveId}`);
+	}
 }
