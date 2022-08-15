d2d9c0ce3f9eee151233d78c4f78d1c07028d14c
Poldi <poldi0106@gmail.com>
2022-08-15 16:39:25 +0200

    added customs to client

--- src/client.ts
+++ src/client.ts
@@ -7,6 +7,7 @@ import UserManager from './structures/manager/UserManager';
 
 import type { Pool } from "mysql";
 import type Redis from "ioredis";
+import CustomManager from './structures/manager/CustomManger';
 
 export default class Client implements BaseClient {
 	constructor(redis: Redis, db: Pool) {
@@ -17,6 +18,7 @@ export default class Client implements BaseClient {
 		this.guilds = new GuildManager(this.cache);
 		this.users = new UserManager(this.cache);
 		this.revives = new ReviveManager(this.cache)
+		this.customs = new CustomManager(this.cache)
 	}
 
 	public pool: Pool;
@@ -26,4 +28,5 @@ export default class Client implements BaseClient {
 	public guilds: GuildManager;
 	public users: UserManager;
 	public revives: ReviveManager;
+	public customs: CustomManager;
 }
