6532675db8fac8789d6c3c1862b14a7176d9775d
Poldi <poldi0106@gmail.com>
2022-09-08 11:05:25 +0200

    bug fix

--- src/api/endpoints/clear.ts
+++ src/api/endpoints/clear.ts
@@ -1,4 +1,6 @@
-export default async function (): Promise<any> {
-	const result = await this.fetchApiEndpoint("clear", "DELETE");
+import { Snowflake } from "discord-api-types/globals";
+
+export default async function (guildId: Snowflake): Promise<any> {
+	const result = await this.fetchApiEndpoint(`clear/${guildId}`, "DELETE");
 	return await result.json();
 }
