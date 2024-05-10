99f4c4f827e06e72293c3aefcf28edc11949aff4
Poldi <poldi0106@gmail.com>
2024-05-10 02:03:48 +0200

    added eslint/prettier/husky

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.strict,
+  {
+	rules: {
+		"@typescript-eslint/no-explicit-any": "error"
+	}
+  }
+);
